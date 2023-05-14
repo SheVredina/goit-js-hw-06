c5d5f04514ca177c301a6da117b2700d4fdc4172
SheVredina <122560380+SheVredina@users.noreply.github.com>
2023-05-14 19:21:28 +0200

    update task 5

---
+++ js/task-05.js
@@ -0,0 +1,17 @@
+const inputEl = document.querySelector('#name-input');
+const textOutput = document.querySelector("#name-output");
+
+
+inputEl.addEventListener('input', (onInput)) ;
+
+function onInput (event) {
+    if(!event.currentTarget.value){
+     return textOutput.textContent = `Anonymous`
+    };
+
+  textOutput.textContent = event.currentTarget.value;
+//   console.log(event);
+ 
+}
+
+     
