c0f97f9d5f6beaac0ea29a340cb4991bac51fdf8
SheVredina <122560380+SheVredina@users.noreply.github.com>
2023-05-15 13:14:19 +0200

    task 7

---
+++ js/task-07.js
@@ -0,0 +1,9 @@
+const inputEl = document.querySelector('#font-size-control');
+const spanEl = document.querySelector('#text');
+
+inputEl.addEventListener('input', onClick);
+
+function onClick(){
+    spanEl.style.fontSize = `${inputEl.value}px`;
+
+}
